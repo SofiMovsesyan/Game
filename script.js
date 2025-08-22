269f020786cac1d382ca75b4aefeda2bf55fa33f
SofiMovsesyan <sofi170707@gmail.com>
2025-08-22 22:09:55 +0400

    table with random numbers

---
+++ script.js
@@ -0,0 +1,42 @@
+rows = 5
+cols = 3
+
+let container = document.getElementById("container")
+
+function createTable(r, c) {
+    let table, tr, td
+    let arr = randomNumbers(r, c)
+    let index = 0
+    table = document.createElement("table")
+    container.appendChild(table)
+    for (let i = 0; i < r; i++) {
+        tr = document.createElement("tr")
+        for (let j = 0; j < c; j++) {
+            td = document.createElement("td")
+            td.style.border = "1px solid black"
+            td.style.width = "50px"
+            td.style.height = "50px"
+            td.innerHTML = arr[index]
+            index++
+            td.style.textAlign = "center"
+            tr.appendChild(td)
+        }
+        table.appendChild(tr)
+    }
+}
+
+function randomNumbers(r, c) {
+    let arr = []
+    for (let i = 1; i <= r * c; i++) {
+        arr.push(i)
+    }
+    for (let i = arr.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr
+
+}
+
+
+createTable(rows, cols)
