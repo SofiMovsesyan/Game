8945558f459f3ba13ea24473a36b6ecd9bc63f6d
SofiMovsesyan <sofi170707@gmail.com>
2025-08-23 16:18:06 +0400

    timer, refresh

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-rows = 5
-cols = 3
+let rows = 5
+let cols = 3
+let win = false
+
+let min = 0
+let s = 0
+let ms = 0
 
 let container = document.getElementById("container")
-
+let btn = document.getElementById("btn")
+let p = document.getElementById("timer")
 let table, tr, td
 
-let btn = document.getElementById("btn")
+let time
 
 btn.addEventListener("click", () => {
+    btn.innerHTML = "REFRESH"
+    timer()
+    createTable(rows, cols)
+    numberClick(rows, cols)
+})
 
-createTable(rows, cols)
-numberClick(rows, cols)
-    })
+function timer() {
+    time = setInterval(() => {
+        if (ms >= 100) {
+            ms = 0
+            s++
+        }
+
+        if (s >= 60) {
+            s = 0
+            min++
+        }
+        p.innerHTML = `Time: ${min}:${s.toString().padStart(2, "0")}.${ms.toString().padStart(2, "0")}`
+
+        ms++
+        // timer()
+    }, 10);
+}
+
 
 function createTable(r, c) {
     let arr = randomNumbers(r, c)
     let index = 0
+    table = container.getElementsByTagName("table") 
+    if (table.length > 0) {
+        table[0].remove()
+    }
     table = document.createElement("table")
     container.appendChild(table)
     for (let i = 0; i < r; i++) {
@@ -57,6 +87,9 @@ function numberClick(r, c) {
                     td.style.backgroundColor = "green"
                     td.classList.add("clicked")
                     count++
+                    if (count == r * c) {
+                        win = true
+                    }
                 }
                 else {
                     td.style.backgroundColor = "red"
